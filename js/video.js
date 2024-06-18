66a20deb20c31b8d2afe54f947d29c2c23d3b900
Peter <peter@thx.network>
2024-06-18 12:24:03 +0200

    Fix video script and load video's from URL

--- js/video.js
+++ js/video.js
@@ -1,43 +1,32 @@
-var player;
+let player;
+
+// Insert YouTube API script
+function initYouTubeAPI() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+ 
+// Create player for given video or default
+function onYouTubeIframeAPIReady() {
+    const defaultVideoId = 'usg5ZkFJIi4';
+    const videoId = new URL(window.location.href).searchParams.get('video');
     
-    function initYouTubeAPI() {
-        var tag = document.createElement('script');
-        tag.src = 'https://www.youtube.com/iframe_api';
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
-
-    function onYouTubeIframeAPIReady() {
-        changeEpisode('YvJxhWcX_VU');
-    }
-
-    function changeEpisode(videoId) {
-        if (player) {
-            player.loadVideoById(videoId);
-            $('#episodes-list li').removeClass('selected');
-            $(`#episodes-list li[data-video-id="${videoId}"]`).addClass('selected');
-        } else {
-            createPlayer(videoId);
+    // Create player
+    player = new window.YT.Player('player', {
+        width: '100%',
+        height: '100%',
+        videoId: videoId || defaultVideoId,
+        events: {
+            // 'onReady': onPlayerReady,
+            // 'onStateChange': onPlayerStateChange
         }
-    }
+    });
 
-    function createPlayer(videoId) {
-        player = new YT.Player('player', {
-            height: '1000',
-            width: '1000',
-            videoId: videoId,
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    }
-
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-    function onPlayerStateChange(event) {
-    }
+    // Set menu item active
+    const menuItem = document.querySelector(`#video-section .sidebar li[data-video-id="${videoId}"]`)
+    menuItem.classList.add('selected');
+}
 
 initYouTubeAPI();
